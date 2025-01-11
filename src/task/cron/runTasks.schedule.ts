79da11891dd80196191cfc88d7dd96d771cf7a3b
tusharjindal <tusharjindal1999@gmail.com>
2025-01-11 11:49:48 +0530

    attempt to limit task number

--- src/task/cron/runTasks.schedule.ts
+++ src/task/cron/runTasks.schedule.ts
@@ -12,23 +12,42 @@ export class TasksSchedule {
 
   @Cron(CronExpression.EVERY_5_SECONDS)
   async handleCron() {
-   console.log("------------------------ inside cron to handle my all tasks--------------------");
 
-    for(let i = 0; i<this.srh.length; i++){
-      console.log("staring to execute task with id: ", this.srh[i]);
-      let data = await this.cacheManager.get(this.srh[i]);
+    let tasks = this.getAllTasksAsPerLimit();
+   console.log(tasks,"------------------------ inside cron to handle my all tasks--------------------");
+
+    for(let i = 0; i<tasks.length; i++){
+     // console.log("staring to execute task with id: ", tasks[i]);
+      let data = await this.cacheManager.get(tasks[i]);
       console.log("Task data data is", data);
       
 
       
-
-      await this.cacheManager.del(this.srh[i]);
-      delete this.srh[i];
+     // console.log("executed task with id: ", tasks[i]);
+      await this.cacheManager.del(tasks[i]);
+      delete tasks[i];
     }
 
   }
 
   getTaskIds(taskId: string){
     this.srh.push(taskId);
+  }
+
+  getAllTasksAsPerLimit() : any {
+    let task = null;
+    if(this.srh.length == 0){
+      return [];
+    }
+    if(this.srh.length <= TASK_PICK_LIMIT){
+      task = this.srh;
+      return task;
+    }else if(this.srh.length > TASK_PICK_LIMIT){
+      for(let i = 0; i<TASK_PICK_LIMIT; i++){
+        task.push(this.srh[i]);
+        delete this.srh[i];
+      }
+      return task;
+    }
   }
 }
